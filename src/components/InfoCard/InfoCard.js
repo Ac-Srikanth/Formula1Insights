3ed1a7fbf7746be3b8696dedcd5b3965b66b0650
Srikanth Ac <acsrikanth24@gmail.com>
2023-09-05 19:59:24 +0530

    Fix: change title in infocard

--- src/components/InfoCard/InfoCard.js
+++ src/components/InfoCard/InfoCard.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import './InfoCard.css'
+import React from 'react';
+import './InfoCard.css';
 
-const InfoCard = ({info}) => {
-    return (
+const InfoCard = ({ info }) => {
+  return (
     <div className="info-card">
-        <div className="info-body">
-            <h3>{info.displaytitle}</h3>
-            <p>{info.description}</p>
-            <p>{info.extract}</p>
-        </div>
-        <div className ="info-footer">
-            <a href={`${info.content_urls.desktop.page}`} rel="noopener noreferrer" target="_blank">More Information</a>
-        </div>   
+      <div className="info-body">
+        <h3>{info.title}</h3>
+        <p>{info.description}</p>
+        <p>{info.extract}</p>
+      </div>
+      <div className="info-footer">
+        <a
+          href={`${info.content_urls.desktop.page}`}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          More Information
+        </a>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default InfoCard
+export default InfoCard;
